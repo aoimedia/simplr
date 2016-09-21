d4321565f5649d49fcbcec9bbb63a9fb3deb6c33
Greg Smith <greg@elmnt.com>
2016-09-21 12:45:32 -0700

    Update index.php

---
+++ app/index.php
@@ -0,0 +1,270 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+<title>simplr</title>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+<meta name="description" content="description">
+<meta name="keywords" content="keywords">
+<meta name="author" content="author">
+<meta name="robots" content="index, nofollow">
+
+<!-- load google fonts & fa icons for the demo -->
+<link href='https://fonts.googleapis.com/css?family=Lato:400,400italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+<!--build:css css/styles.min.css -->
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<!-- endbuild -->
+
+</head>
+<body>
+  
+<header>
+<div class="container container--header">
+<div class="wrap--single">
+<div class="grid">
+
+  <div class="content align--center">
+    <h1 class="intro__title">simplr</h1>
+    <h4 class="intro__subtitle">A lightweight, no-float grid &amp; typography starter kit with a minified build CSS file of 3.3KB.</h4>
+  </div>
+
+</div><!-- /.grid -->
+</div><!-- /.wrap -->
+</div><!-- /.container -->
+</header>
+
+<!-- type -->
+
+<div class="container" style="padding-top:4rem;">
+<div class="wrap">
+<div class="grid">
+
+  <div class="grid__col--100">
+    <div class="content">
+      <h1>h1 Lorem ipsum</h1>
+      <h2>h2 Lorem ipsum</h2>
+      <h3>h3 Lorem ipsum</h3>
+      <h4>h4 Lorem ipsum</h4>
+      <h5>h5 Lorem ipsum</h5>
+    </div>
+  </div>
+
+</div><!-- /.grid -->
+</div><!-- /.wrap -->
+</div><!-- /.container -->
+
+<!-- /type -->
+
+<div class="container">
+<div class="wrap">
+<div class="grid">
+
+  <div class="grid__col--50">
+    <div class="content">
+      <h2>50 Lorem ipsum</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+  --><div class="grid__col--50">
+    <div class="content">
+      <h2>50 Lorem ipsum</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+
+</div><!-- /.grid -->
+</div><!-- /.wrap -->
+</div><!-- /.container -->
+
+<div class="container">
+<div class="wrap">
+<div class="grid">
+  <div class="grid__col--30">
+    <div class="content">
+    <h2>30 Lorem ipsum</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--70">
+    <div class="content">
+    <h2>70 Lorem ipsum</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+</div><!-- /.grid -->
+</div><!-- /.wrap -->
+</div><!-- /.container -->
+
+
+<div class="container">
+<div class="wrap">
+<div class="grid">
+
+  <div class="grid__col--40">
+    <div class="content">
+    <h2>40 Lorem ipsum</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--60">
+    <div class="content">
+    <h2>60 Lorem ipsum</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+
+</div><!-- /.grid -->
+</div><!-- /.wrap -->
+</div><!-- /.container -->
+
+<div class="container">
+<div class="wrap">
+<div class="grid">
+
+  <div class="grid__col--333">
+    <div class="content">
+    <h3>33 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--333">
+    <div class="content">
+    <h3>33 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--333">
+    <div class="content">
+    <h3>33 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+
+</div><!-- /.grid -->
+</div><!-- /.wrap -->
+</div><!-- /.container -->
+
+<div class="container">
+<div class="wrap">
+<div class="grid">
+
+  <div class="grid__col--33">
+    <div class="content">
+    <h2>33 Lorem ipsum</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--67">
+    <div class="content">
+    <h2>67 Lorem ipsum</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+
+</div><!-- /.grid -->
+</div><!-- /.wrap -->
+</div><!-- /.container -->
+
+<div class="container">
+<div class="wrap">
+<div class="grid">
+
+  <div class="grid__col--25">
+    <div class="content">
+    <h3>25 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--25">
+    <div class="content">
+    <h3>25 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--25">
+    <div class="content">
+    <h3>25 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--25">
+    <div class="content">
+    <h3>25 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+
+</div><!-- /.grid -->
+</div><!-- /.wrap -->
+</div><!-- /.container -->
+
+<div class="container" style="padding-bottom:4rem;">
+<div class="wrap">
+<div class="grid">
+
+  <div class="grid__col--20">
+    <div class="content">
+    <h3>20 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--20">
+    <div class="content">
+    <h3>20 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--20">
+    <div class="content">
+    <h3>20 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--20">
+    <div class="content">
+    <h3>20 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div><!--
+--><div class="grid__col--20">
+    <div class="content">
+    <h3>20 Lorem ipsum</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+
+</div><!-- /.grid -->
+</div><!-- /.wrap -->
+</div><!-- /.container -->
+
+<footer>
+<div class="container container--footer">
+<div class="wrap--single">
+<div class="grid">
+
+  <div class="content align--center">
+    <p>made by <a href="https://elmnt.com/" target="_blank">elmnt</a></p>
+    <p><a href="https://twitter.com/elmnt" target="_blank"><i class="fa fa-twitter"></i></a><a href="https://github.com/elmnt" target="_blank"><i class="fa fa-github-alt"></i></a></p>
+  </div>
+
+</div><!-- /.grid -->
+</div><!-- /.wrap -->
+</div><!-- /.container -->
+</footer>
+
+<!--build:js js/main.min.js -->
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/main.js"></script>
+<!-- endbuild -->
+
+<script type="text/javascript">
+$(document).ready(function () {
+});
+</script>
+
+</body>
+</html>
